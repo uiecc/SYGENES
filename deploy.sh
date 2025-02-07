275aa2ed91fe2f25f535a24fdf59191b869a005f
Emane bile <davyemane1@gmail.com>
2025-02-07 11:20:18 +0100

    last update

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ cd domains/sygenes.esign.cm/public_html || exit 1
 git pull --quiet || exit 1
 
 # Exécuter les migrations en forçant leur application
-php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration --force || exit 1
+php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration || exit 1
 
 # Mettre à jour les dépendances Composer sans interaction et sans erreur bloquante
 php composer.phar update --no-interaction --no-progress --prefer-dist || exit 1
