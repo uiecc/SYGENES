b5a4468d53976e823890f1612a076c3b1750f0a4
Emane bile <davyemane1@gmail.com>
2025-02-08 15:15:21 +0100

    force update

--- push.sh
+++ push.sh
@@ -1,3 +1,11 @@
+# D'abord, assurez-vous d'être sur la branche main
+git checkout main
+
+# Ajoutez tous les changements
 git add .
-git commit -m "last update"
-git push 
+
+# Faites un commit
+git commit -m "force update"
+
+# Forcez le push
+git push -f origin main
