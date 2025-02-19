7e7042f59a78949e893d15dabeb41eb3d3208994
uiecc <uieccesign@gmail.com>
2025-02-19 20:38:33 +0100

    force update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
+const path = require('path'); // Add this line to require the path module
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
@@ -6,30 +7,22 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 
 Encore
     .enablePostCssLoader()
-
     .setOutputPath('public/build/')
     .setPublicPath('/build')
-    .enableStimulusPlugin()
     .addEntry('app', './assets/app.js')
     .splitEntryChunks()
-
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-
     .enableSingleRuntimeChunk()
     .enableStimulusBridge('./assets/controllers.json')
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
-    .configureResolve({
-        alias: {
-            '@symfony/stimulus-bridge': path.resolve(__dirname, 'node_modules/@symfony/stimulus-bridge'),
-        },
+    .addAliases({
+        '@symfony/stimulus-bridge': path.resolve(__dirname, 'node_modules/@symfony/stimulus-bridge'),
     })
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
         config.corejs = '3.38';
-    })
-;
+    });
 
 module.exports = Encore.getWebpackConfig();
